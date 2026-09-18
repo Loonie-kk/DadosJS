08ab6248524c2ff8404bfa786e290d3dcb0343d3
Loonie-kk <valezziluana@gmail.com>
2026-09-18 13:55:16 -0300

    Dados no JS

---
+++ textos.js
@@ -0,0 +1,53 @@
+// 1- String (Textos)
+
+
+//Exercício 1 - Crie uma variável nome com seu nome e exiba no console.
+let nome = "Luana";
+console.log("------------------");
+
+//Exercício 2 - Crie uma variável sobrenome e mostre nome + sobrenome.
+let sobrenome = " Alves Valezzi";
+console.log(`Meu nome é: ${nome + sobrenome} `);
+console.log("------------------");
+
+//Exercício 3 - Crie uma frase usando Template String.
+let frase = "Desenhar"
+console.log(`Meu hobby é ${frase} ` );
+console.log("------------------");
+
+//Exercício 4 - Guarde o nome de uma escola e exiba.
+let escola = "Sesi"
+console.log(escola);
+console.log("------------------");
+
+//Exercício 5 - Crie duas strings e junte em uma frase.
+let fraseUm = "Sei"
+let fraseDois = " lá"
+console.log(fraseUm + fraseDois)
+console.log("------------------");
+
+//Exercício 6 - Exiba uma mensagem de boas-vindas.
+let saudacao = "Olá, " + nome + "! Boas-Vindas";
+console.log(saudacao);
+console.log("------------------");
+
+//Exercício 7 - Mostre o tamanho de uma string.
+let tamanho = "Ferrou";
+console.log(tamanho.length);
+console.log("------------------");
+
+//Exercício 8 - Transforme um texto em letras maiúsculas.
+let textoUm= "You call on me as bells begin to chime";
+console.log(textoUm.toUpperCase()); //Maiúscula
+console.log("------------------");
+
+//Exercício 9 - Transforme um texto em letras minúsculas.
+let textoDois = "I can see through the scars inside you"
+console.log(textoDois.toLowerCase()); //Minúscula
+console.log("------------------");
+
+//Exercício 10 - Crie uma frase com nome e cidade.
+let cidade = "Guarulhos"
+console.log(`Meu nome é ${nome} e moro em ${cidade}`);
+console.log("------------------");
+
