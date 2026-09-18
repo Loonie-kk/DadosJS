728baf0a113c75a923923eb6ddbd28f232c2c69b
Loonie-kk <valezziluana@gmail.com>
2026-09-18 14:22:46 -0300

    Dados no JS

---
+++ numeros.js
@@ -0,0 +1,58 @@
+// Exercício 1 - Crie dois números e exiba a soma.
+let soma = 40 + 13;
+console.log(soma);
+console.log("------------------");
+
+// Exercício 2 - Calcule a subtração.
+let subtracao = 5 - 92;
+console.log(subtracao);
+console.log("------------------");
+
+
+// Exercício 3 - Calcule a multiplicação.
+let multiplicacao = 5 * 12;
+console.log(multiplicacao);
+console.log("------------------");
+
+
+// Exercício 4 - Calcule a divisão.
+let divisao = 5 * 12;
+console.log(divisao);
+console.log("------------------");
+
+
+
+// Exercício 5 - Calcule a média de dois números.
+let somaDois = 19 + 12;
+let media = somaDois / 2
+console.log(media);
+console.log("------------------");
+
+// Exercício 6 - Calcule o dobro de um número.
+let dobro = 19 * 19;
+console.log(dobro);
+console.log("------------------");
+
+// Exercício 7 - Calcule o resto da divisão.
+let resultado = divisao * 60 ;
+let resto = divisao - resultado;
+console.log(resto);
+console.log("------------------");
+
+// Exercício 8 - Crie um preço e um desconto.
+let preco = 70; 
+let resultadoUm = preco * 0o5;
+let desconto = resultadoUm - 70
+console.log(desconto);
+console.log("------------------");
+
+// Exercício 9 - Converta idade em meses.
+let idade =  7 * 12
+let mes = idade + 2
+console.log(mes);
+console.log("------------------");
+
+// Exercício 10 - Calcule o quadrado de um número.3
+let quadrado= 3 * 3;
+console.log(quadrado);
+console.log("------------------");
